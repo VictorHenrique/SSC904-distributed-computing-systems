a7d3c50a78b0723e9670f4d0d6caf705739f8e06
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2023-06-27 14:08:25 -0300

    creating main file

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import rpyc
-import uuid
 import math
-import random
-import configparser
-import signal
-import pickle
-import sys
-import os
-
+import rpyc
 from rpyc.utils.server import ThreadedServer
+import random
+import uuid
 
 BLOCK_SIZE = 100
 REPLICATION_FACTOR = 3
@@ -17,58 +11,42 @@ MINIONS = {"1": ("127.0.0.1", 8000),
            "3": ("127.0.0.1", 9000),
            "4": ("127.0.0.1", 9500)}
 
-class MasterService(rpyc.Service):
-    """
-    file_block = {'file.txt': ["block1", "block2"]}
-    block_minion = {"block1": [1,3]}
-    minions = {"1": (127.0.0.1,8000), "3": (127.0.0.1,9000)}
-    """
+class MainService(rpyc.Service):
+    def __init__(self) -> None:
+        self.file_block = {}
+        self.block_minion = {}
+        self.minions = MINIONS
 
-    file_block = {}
-    block_minion = {}
-    minions = MINIONS
+        self.block_size = BLOCK_SIZE
+        self.replication_factor = REPLICATION_FACTOR
 
-    block_size = BLOCK_SIZE
-    replication_factor = REPLICATION_FACTOR
-
-    def exposed_read(self, file):
-
+    def read(self, file):
         mapping = []
-        # iterate over all of file's blocks
         for blk in self.file_block[file]:
             minion_addr = []
-            # get all minions that contain that block
             for m_id in self.block_minion[blk]:
                 minion_addr.append(self.minions[m_id])
 
             mapping.append({"block_id": blk, "block_addr": minion_addr})
         return mapping
 
-    def exposed_write(self, file, size):
-
+    def write(self, file, size):
         self.file_block[file] = []
-
         num_blocks = int(math.ceil(float(size) / self.block_size))
         return self.alloc_blocks(file, num_blocks)
 
     def alloc_blocks(self, file, num_blocks):
         return_blocks = []
-        for i in range(0, num_blocks):
-            block_id = str(uuid.uuid1()) # generate a block
-            minion_ids = random.sample(     # allocate REPLICATION_FACTOR number of minions
-                list(self.minions.keys()), self.replication_factor)
+        for _ in range(num_blocks):
+            block_id = str(uuid.uuid1()) 
+            minion_ids = random.sample(list(self.minions.keys()), self.replication_factor)
             minion_addr = [self.minions[m] for m in minion_ids]
             self.block_minion[block_id] = minion_ids
             self.file_block[file].append(block_id)
 
-            return_blocks.append(
-                {"block_id": block_id, "block_addr": minion_addr})
-
+            return_blocks.append({"block_id": block_id, "block_addr": minion_addr})
         return return_blocks
 
-
 if __name__ == "__main__":
-    t = ThreadedServer(MasterService(), port=2131, protocol_config={
-    'allow_public_attrs': True,
-})
+    t = ThreadedServer(MainService(), port=2131, protocol_config={'allow_public_attrs': True})
     t.start()
